cbc3f43e6b49eff7d6ac66103b8cc852e7152ed8
oneofthezombies <hunhoekim@gmail.com>
2025-01-14 06:39:40 +0000

    wip

--- crates/ntex/src/main.rs
+++ crates/ntex/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    todo!();
+use std::env;
+
+use ntex::web;
+
+#[web::get("/")]
+async fn hello() -> impl web::Responder {
+    web::HttpResponse::Ok().body("Hello world!")
+}
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+    let port: u16 = env::var("PORT").unwrap().parse().unwrap();
+    web::HttpServer::new(|| web::App::new().service(hello))
+        .bind(("127.0.0.1", port))?
+        .run()
+        .await
 }
