e0043a0a0f7dc7b8b986819b8b4f1df1344df72f
oneofthezombies <hunhoekim@gmail.com>
2025-01-14 07:45:12 +0000

    wip

--- crates/custom/src/main.rs
+++ crates/custom/src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::net::SocketAddr;
+use std::thread;
+
+use socket2::Domain;
+use socket2::Protocol;
+use socket2::Socket;
+use socket2::Type;
+
 fn main() {
-    todo!();
+    let port: u16 = env::var("PORT").unwrap().parse().unwrap();
+    let worker_count: usize = 8;
+    let backlog: i32 = 2048;
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    (0..worker_count)
+        .fold(Vec::with_capacity(worker_count), |mut acc, _| {
+            let worker = thread::spawn(move || {
+                let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP)).unwrap();
+                socket.set_reuse_port(true).unwrap();
+                socket.bind(&addr.into()).unwrap();
+                socket.listen(backlog).unwrap();
+            });
+            acc.push(worker);
+            acc
+        })
+        .into_iter()
+        .for_each(|x| x.join().unwrap());
 }
--- crates/ntex/src/main.rs
+++ crates/ntex/src/main.rs
@@ -10,10 +10,13 @@ async fn hello() -> impl web::Responder {
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
     let port: u16 = env::var("PORT").unwrap().parse().unwrap();
+    let worker_count: usize = 8;
+    let backlog: i32 = 2048;
+
     web::HttpServer::new(|| web::App::new().service(hello))
         .bind(("127.0.0.1", port))?
-        .backlog(2048)
-        .workers(8)
+        .backlog(backlog)
+        .workers(worker_count)
         .maxconn(25_000)
         .maxconnrate(256)
         .keep_alive(5)
